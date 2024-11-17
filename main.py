d1e0cd288a786e56fc739521f75d43ded7ba9b5d
Brombeertje69 <109145098+Brombeertje69@users.noreply.github.com>
2024-11-17 15:47:30 +0100

    Update main.py with extra case

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ poseidon(
     output_path='output/example_with_classes.png'
 )
 
-# Example 3
+# Example 3: Exclude private/external
 poseidon(
     folder_path="examples/example_ignore",
     graph_type='call',
-    output_path='output/example_ignore_show_all.png'
+    output_path='output/example_ignore_exclude_all.png'
 )
 
 
-# Example 4
+# Example 4: Show private
 poseidon(
     folder_path="examples/example_ignore",
     graph_type='call',
@@ -35,9 +35,19 @@ poseidon(
     exclude_private=False,
 )
 
+# Example 5: Show External
 poseidon(
     folder_path="examples/example_ignore",
     graph_type='call',
     output_path='output/example_show_external.png',
     exclude_external=False,
+)
+
+# Example 6: Show all
+poseidon(
+    folder_path="examples/example_ignore",
+    graph_type='call',
+    output_path='output/example_show_all.png',
+    exclude_external=False,
+    exclude_private=False
 )
