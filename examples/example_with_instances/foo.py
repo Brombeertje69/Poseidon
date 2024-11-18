cb3ed6e055208af30f807036ac4f1e02d91663b2
Brombeertje69 <109145098+Brombeertje69@users.noreply.github.com>
2024-11-18 21:18:25 +0100

    Change example with instances

--- examples/example_with_instances/foo.py
+++ examples/example_with_instances/foo.py
@@ -1,3 +1,5 @@
+from .cat import Cat, Ragdoll
+
 class Dog:
     def bark(self):
         return 'Woof!'
@@ -7,12 +9,12 @@ class Dog:
 
 # Dog is being passed to the owner
 class Owner:
-    def __init__(self, dog: Dog):
-        self.dog = dog
+    def __init__(self, labrador: Dog):
+        self.labrador = labrador
 
     def pet_dog(self):
         # Call to instance method!
-        self.dog.pet()
+        self.labrador.pet()
 
 # Dog is being created in the owner
 class Owner2:
@@ -22,6 +24,17 @@ class Owner2:
     def pet_dog(self):
         # Call to instance method!
         self.dog.pet()
+
+class Owner3:
+    def pet_dog(self, golden_retriever: Dog):
+        # Call to instance method!
+        golden_retriever.pet()
+
+# Cat is defined in other file & as a local variable
+class CatOwner:
+    def pet_cat(self):
+        cat = Cat()
+        cat.pet()
 
 
 # Function calling a defintion
